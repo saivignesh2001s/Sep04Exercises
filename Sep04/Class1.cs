e6b2bd6a749416b404e69db12914761f2a9432a8
saivignesh2001s <saivignesh2001s@gmail.com>
2022-09-06 16:14:26 +0530

    Add project files.

---
+++ Sep04/Class1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Sep04
+{
+    public abstract class Person
+    {
+        public abstract void work();
+        public void spot()
+        {
+            Console.WriteLine("fav destination is Chennai");
+        }
+    }
+    public class Employee : Person
+    {
+        public override void work()
+        {
+            Console.WriteLine("I am employee");
+        }
+    }
+    public class Manager : Person
+    {
+        public override void work()
+        {
+            Console.WriteLine("I am manager");
+        }
+
+    }
+    public class Clerk : Person
+    {
+        public override void work()
+        {
+            Console.WriteLine("I am clerk");
+        }
+    }
+    interface Ibank
+    {
+        void CalculateInterest();
+        void PrintBalance();
+    }
+    public class Bank : Ibank
+    {
+        private int initialbalance = 500;
+        private int deposit;
+        public int Deposit
+        {
+            get { return deposit; }
+            set { deposit = value; }
+        }
+        private int withdrawal;
+        public int Withdraw
+        {
+            get { return withdrawal; }
+            set
+            {
+                if (total() < 500)
+                {
+                    withdrawal = 0;
+                }
+                {
+                    withdrawal = value;
+                }
+            }
+        }
+        public int total()
+        {
+            int Total = 0;
+            Total = deposit + initialbalance - withdrawal;
+            return Total;
+        }
+
+        private DateTime depositdate;
+        public DateTime Depositdate
+        {
+            get { return depositdate; }
+            set { depositdate = value; }
+        }
+        public void Noofdays()
+        {
+            TimeSpan t = DateTime.Now - depositdate;
+            Console.WriteLine($"Number of days{t.TotalDays}");
+        }
+
+        public void CalculateInterest()
+        {
+            int s = total();
+            Console.WriteLine("Enter interest rate");
+            int m = Convert.ToInt32(Console.ReadLine());
+            float Interest = (s * m) / 100;
+            Console.WriteLine($"Interest rate per year is {Interest}");
+        }
+
+        public void PrintBalance()
+        {
+            int n = total();
+            Console.WriteLine($"Balance is {n}");
+        }
+    }
+    public class Savings : Bank, Ibank
+    {
+
+        public void CalculateInterest()
+        {
+            int m = base.total();
+            float Interest = (4 * m) / 100;
+            Console.WriteLine($"Savings account Interest is {Interest}");
+        }
+
+        public void PrintBalance()
+        {
+            int m = base.total();
+            float Interest = (4 * m) / 100;
+            float ni = m + Interest;
+            Console.WriteLine($"Balance is {ni}");
+        }
+    }
+    public class Current : Bank, Ibank
+    {
+
+        public void CalculateInterest()
+        {
+            int m = base.total();
+            float Interest = (2 * m) / 100;
+            Console.WriteLine($"Current account Interest is {Interest}");
+        }
+
+        public void PrintBalance()
+        {
+            int m = base.total();
+            float Interest = (2 * m) / 100;
+            float ni = m + Interest;
+            Console.WriteLine($"Balance is {ni}");
+        }
+    }
+    public static class extension{
+        
+        public static int deposit(this int i)
+        {
+            return i;
+        }
+        public static int with(this int i)
+        {
+            return i;
+        }
+        }
+}
---
+++ Sep04Exercises/Program.cs
@@ -0,0 +1,55 @@
+﻿using Sep04;
+using System.Security.Cryptography;
+
+namespace Sep04Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager m=new Manager();
+            m.work();
+            Employee emp=new Employee();
+            emp.work();
+            Clerk ck=new Clerk();
+            ck.work();
+            m.spot();
+            Bank b = new Bank();
+            Console.WriteLine("Enter deposit year,month,date");
+            int Ye = Convert.ToInt32(Console.ReadLine());
+            int Mo = Convert.ToInt32(Console.ReadLine());
+            int kg = Convert.ToInt32(Console.ReadLine());
+            DateTime mo = new DateTime(Ye, Mo, kg);
+            b.Depositdate = mo;
+            b.Noofdays();
+            Console.WriteLine("Enter deposit");
+            b.Deposit = Convert.ToInt32(Console.ReadLine());
+            b.PrintBalance();
+            b.CalculateInterest();
+            Console.WriteLine("Enter Withdrwal");
+            b.Withdraw = Convert.ToInt32(Console.ReadLine());
+            b.PrintBalance();
+            b.CalculateInterest();
+            Savings s = new Savings();
+            Console.WriteLine("Savings Dep amount");
+            int mi = Convert.ToInt32(Console.ReadLine());
+            s.Deposit= mi.deposit();
+            Console.WriteLine("Withdraw amount from savings account");
+            int im = Convert.ToInt32(Console.ReadLine());
+            s.Withdraw = im.with();
+            s.CalculateInterest();
+            s.PrintBalance();
+            Current c=new Current();
+            Console.WriteLine("Current Dep amount");
+            int pi = Convert.ToInt32(Console.ReadLine());
+            c.Deposit = pi.deposit();
+            Console.WriteLine("Current Withdraw amount");
+            int ip = Convert.ToInt32(Console.ReadLine());
+            c.Withdraw = ip.with();
+            c.CalculateInterest();
+            c.PrintBalance();
+
+            Console.ReadLine();
+        }
+    }
+}
